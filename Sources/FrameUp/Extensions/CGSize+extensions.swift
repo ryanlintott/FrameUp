1704716f1b98f6928e1a4b038aae32f5e1134159
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2021-09-16 18:48:36 -0400

    Removed CGSize Proportional conformance

--- Sources/FrameUp/Extensions/CGSize+extensions.swift
+++ Sources/FrameUp/Extensions/CGSize+extensions.swift
@@ -1,10 +1,40 @@
 //
-//  CGSize+extensions.swift
-//  FrameUp
+//  SwiftUIView.swift
+//  
 //
-//  Created by Ryan Lintott on 2020-09-21.
+//  Created by Ryan Lintott on 2021-09-16.
 //
 
 import SwiftUI
 
-extension CGSize: Proportionable { }
+internal struct ProportionableSize: Proportionable {
+    var width: CGFloat
+    var height: CGFloat
+    
+    init(_ cgSize: CGSize) {
+        width = cgSize.width
+        height = cgSize.height
+    }
+}
+
+internal extension CGSize {
+    var proportionableSize: ProportionableSize {
+        ProportionableSize(self)
+    }
+    
+    var aspectFormat: AspectFormat {
+        proportionableSize.aspectFormat
+    }
+    
+    var aspectRatio: CGFloat {
+        proportionableSize.aspectRatio
+    }
+
+    var minDimension: CGFloat {
+        proportionableSize.minDimension
+    }
+
+    var maxDimension: CGFloat {
+        proportionableSize.maxDimension
+    }
+}
--- Sources/FrameUp/OverlappingImage.swift
+++ Sources/FrameUp/OverlappingImage.swift
@@ -34,11 +34,13 @@ public struct OverlappingImage: View {
     }
     
     public init(uiImage: UIImage, top: CGFloat = 0, bottom: CGFloat = 0) {
-        self.init(Image(uiImage: uiImage), aspectRatio: uiImage.size.aspectRatio, top: top, bottom: bottom)
+        let aspectRatio = uiImage.size.width / uiImage.size.height
+        self.init(Image(uiImage: uiImage), aspectRatio: aspectRatio, top: top, bottom: bottom)
     }
     
     public init(uiImage: UIImage, left: CGFloat = 0, right: CGFloat = 0) {
-        self.init(Image(uiImage: uiImage), aspectRatio: uiImage.size.aspectRatio, left: left, right: right)
+        let aspectRatio = uiImage.size.width / uiImage.size.height
+        self.init(Image(uiImage: uiImage), aspectRatio: aspectRatio, left: left, right: right)
     }
     
     public var body: some View {
