d2ae8282c2c55dadf911bbebe81110cf66edfdb2
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2022-09-12 00:20:49 -0400

    Added accessibility actions to TabMenuView through NamedAction

---
+++ Sources/FrameUp/TabMenu/NamedAction.swift
@@ -0,0 +1,20 @@
+//
+//  LabelledAction.swift
+//  
+//
+//  Created by Ryan Lintott on 2022-09-11.
+//
+
+import SwiftUI
+
+public struct NamedAction {
+    public let name: Text
+    public let action: () -> Void
+}
+
+public extension NamedAction {
+    init(_ name: LocalizedStringKey, action: @escaping () -> Void) {
+        self.name = Text(name)
+        self.action = action
+    }
+}
