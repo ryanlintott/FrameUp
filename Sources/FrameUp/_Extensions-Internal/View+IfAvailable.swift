bffb015fbf296cdcc168a48d5cf4abf89d1dada3
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2023-08-11 17:51:45 -0400

    Fixed optionalScrolling in SmartScrollView in iOS 17 so it uses scrollBounceBehavior Added ifAvailable View extension for internal use

---
+++ Sources/FrameUp/_Extensions-Internal/View+IfAvailable.swift
@@ -0,0 +1,20 @@
+//
+//  View+IfAvailable.swift
+//  DragAndDrop
+//
+//  Created by Ryan Lintott on 2023-07-14.
+//
+
+import SwiftUI
+
+extension View {
+    /// Applies the given transform.
+    ///
+    /// Useful for availability branching on view modifiers. Do not branch with any properties that may change during runtime as this will cause errors.
+    /// - Parameters:
+    ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: The view transformed by the transform.
+    func ifAvailable<Content: View>(@ViewBuilder _ transform: (Self) -> Content) -> some View {
+        transform(self)
+    }
+}
