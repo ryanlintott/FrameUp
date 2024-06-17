defeadd95eea9d396cf36c4b2383cb6d0c4ff12a
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2024-06-17 15:49:58 -0400

    Added Sendable to FUViewThatFits

--- Sources/FrameUp/FULayout/FULayouts/FUViewThatFits.swift
+++ Sources/FrameUp/FULayout/FULayouts/FUViewThatFits.swift
@@ -28,7 +28,7 @@ import SwiftUI
  
  (`.fixedSize` needs to be used in this example or the first view will automatically fit by truncating the text)
  */
-public struct FUViewThatFits: FULayout {
+public struct FUViewThatFits: FULayout, Sendable {
     public let maxItemWidth: CGFloat?
     public let maxItemHeight: CGFloat?
     public let fixedSize: Axis.Set
