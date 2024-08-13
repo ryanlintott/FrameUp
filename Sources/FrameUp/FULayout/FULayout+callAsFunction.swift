5d9716893a3fd4273a73f3309c6be07215aa9e54
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2024-08-13 10:16:05 -0400

    VariadicView concurrency issues fixed by Apple in beta 5 so workaround has been removed.

--- Sources/FrameUp/FULayout/FULayout+callAsFunction.swift
+++ Sources/FrameUp/FULayout/FULayout+callAsFunction.swift
@@ -38,10 +38,6 @@ fileprivate struct _FULayoutView<Content: View, L: FULayout>: View {
     }
 }
 
-/// Old simpler version that worked before Swift 6
-/// FB13869223
-/// body(childen:) on _VariadicView_MultiViewRoot is nonisolated. If it were isolated to the main actor this would work
-#if compiler(<6)
 fileprivate struct _VariadicFULayoutRoot<L: FULayout>: _VariadicView_MultiViewRoot {
     let layout: L
     let contentOffsets: [Int: CGPoint]
@@ -62,47 +58,5 @@ fileprivate struct _VariadicFULayoutRoot<L: FULayout>: _VariadicView_MultiViewRo
         }
     }
 }
-
-#else
-/// Workaround
-@MainActor
-fileprivate struct _VariadicFULayoutRoot<L: FULayout, Body: View>: _VariadicView_MultiViewRoot {
-    @ViewBuilder
-    nonisolated(unsafe) var content: (_VariadicView.Children) -> Body
-    
-    @ViewBuilder
-    nonisolated func body(children: _VariadicView.Children) -> some View {
-        content(children)
-    }
-}
-
-fileprivate struct _VariadicFULayoutRootContent<L: FULayout>: View {
-    let layout: L
-    let contentOffsets: [Int: CGPoint]
-    let children: _VariadicView.Children
-    
-    init(layout: L, contentOffsets: [Int : CGPoint], children: _VariadicView.Children) {
-        self.layout = layout
-        self.contentOffsets = contentOffsets
-        self.children = children
-    }
-    
-    var defaultOffset: CGPoint {
-        contentOffsets.first?.value ?? .zero
-    }
-    
-    var body: some View {
-        ForEach(Array(zip(children, children.indices)), id: \.0.id) { (child, index) in
-            FULayoutChildView(layout: layout, index: index, contentOffset: contentOffsets[index], defaultOffset: defaultOffset, content: child)
-        }
-    }
-}
-
-extension _VariadicFULayoutRoot where Body == _VariadicFULayoutRootContent<L> {
-    init(_ layout: L, contentOffsets: [Int : CGPoint]) {
-        self.init { _VariadicFULayoutRootContent(layout: layout, contentOffsets: contentOffsets, children: $0) }
-    }
-}
-#endif
 
 
