3b46ddcfc02a85ed060bb0c9e7ab14a7f90f415a
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2024-11-29 14:14:39 -0500

    Update to internal onPreferenceChange function so it runs on the main actor in Swift 5 as well.

--- Sources/FrameUp/_Extensions-Internal/View+onPreferenceChange.swift
+++ Sources/FrameUp/_Extensions-Internal/View+onPreferenceChange.swift
@@ -9,14 +9,10 @@ import SwiftUI
 
 internal extension View {
     @preconcurrency @inlinable nonisolated func onPreferenceChangeMainActor<K>(_ key: K.Type = K.self, perform action: @escaping @MainActor (K.Value) -> Void) -> some View where K : PreferenceKey, K.Value : Equatable & Sendable {
-        #if swift(>=6)
         onPreferenceChange(key) { newValue in
             Task { @MainActor in
                 action(newValue)
             }
         }
-        #else
-        onPreferenceChange(key, perform: action)
-        #endif
     }
 }
