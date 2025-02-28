6b7c70656191313b10e61e32ee847cc265426312
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2025-02-28 11:34:12 -0500

    Added additional onPreferenceChange extensions to work around nonisolated closure. Improved WidthReaderExample so it changes with animation.

--- Example/FrameUpExample/FrameAdjustmentExamples/WidthReaderExample.swift
+++ Example/FrameUpExample/FrameAdjustmentExamples/WidthReaderExample.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct WidthReaderExample: View {
     @State private var percent = 0.7
+    @State private var padding = 0
     
     var body: some View {
         VStack {
@@ -26,24 +27,35 @@ struct WidthReaderExample: View {
                 .foregroundColor(.white)
                 .background(Color.blue)
             }
+            .padding(.horizontal, CGFloat(padding))
+            .animation(.spring(), value: padding)
             
-            Text("The WidthReader above does not have a fixed height and will fit the content.")
-                .padding()
-            
-            Button("Animate") {
-                withAnimation {
-                    percent = .random(in: 0...1)
+            VStack {
+                Text("The WidthReader above does not have a fixed height and will fit the content.")
+                
+                HStack {
+                    Button("Percent \(percent * 100, specifier: "%.0f")%") {
+                        withAnimation {
+                            percent = .random(in: 0...1)
+                        }
+                    }
+                    #if os(tvOS)
+                    Button("-") { percent = max(0, percent - 0.1) }
+                    Button("+") { percent = min(1, percent + 0.1) }
+                    #else
+                    Slider(value: $percent, in: 0...1)
+                    #endif
                 }
-            }
-            
-            HStack {
-                Text("Percent \(percent * 100, specifier: "%.0f")%")
-                #if os(tvOS)
-                Button("-") { percent = max(0, percent - 0.1) }
-                Button("+") { percent = min(1, percent + 0.1) }
-                #else
-                Slider(value: $percent, in: 0...1)
-                #endif
+                
+                HStack {
+                    Stepper(value: $padding, in: 0...100, step: 10) {
+                        Button("Padding \(padding)") {
+//                            withAnimation {
+                                padding = Int.random(in: 0...100)
+//                            }
+                        }
+                    }
+                }
             }
             .padding()
         }
--- Sources/FrameUp/_Extensions-Internal/View+onPreferenceChange.swift
+++ Sources/FrameUp/_Extensions-Internal/View+onPreferenceChange.swift
@@ -1,5 +1,5 @@
 //
-//  SwiftUIView.swift
+//  View+onPreferenceChange.swift
 //  FrameUp
 //
 //  Created by Ryan Lintott on 2024-11-28.
@@ -7,7 +7,36 @@
 
 import SwiftUI
 
+struct OnPreferenceChangeUpdatingBinding<K>: ViewModifier where K : PreferenceKey, K.Value : Equatable & Sendable {
+    let key: K.Type
+    @Binding var value: K.Value
+    var predicate: @Sendable (_ currentValue: K.Value, _ newValue: K.Value) -> Bool
+    
+    func body(content: Content) -> some View {
+        content
+            .onPreferenceChange(key) { [$value] newValue in
+                if predicate($value.wrappedValue, newValue) {
+                    $value.wrappedValue = newValue
+                }
+            }
+    }
+}
+
 internal extension View {
+    /// Updating a binding based on a predicate.
+    @preconcurrency nonisolated func onPreferenceChange<K>(_ key: K.Type = K.self, update value: Binding<K.Value>, where predicate: @escaping @Sendable (_ oldValue: K.Value, _ newValue: K.Value) -> Bool) -> some View where K : PreferenceKey, K.Value : Equatable & Sendable {
+        modifier(OnPreferenceChangeUpdatingBinding(key: key, value: value, predicate: predicate))
+        
+    }
+    
+    /// Updating a binding on every change.
+    @preconcurrency @inlinable nonisolated func onPreferenceChange<K>(_ key: K.Type = K.self, update value: Binding<K.Value>) -> some View where K : PreferenceKey, K.Value : Equatable & Sendable {
+        onPreferenceChange(key) { newValue in
+            value.wrappedValue = newValue
+        }
+    }
+    
+    /// Wrap the action in a MainActor Task.
     @preconcurrency @inlinable nonisolated func onPreferenceChangeMainActor<K>(_ key: K.Type = K.self, perform action: @escaping @MainActor (K.Value) -> Void) -> some View where K : PreferenceKey, K.Value : Equatable & Sendable {
         onPreferenceChange(key) { newValue in
             Task { @MainActor in
